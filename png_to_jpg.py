be68545b4ac4bc5646bedce8cb0e8d4be44d2757
Stella-67 <1302039477@qq.com>
2025-10-07 20:10:54 -0400

    update

--- png_to_jpg.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-# Convert PNG to JPG
-def convert_png_to_jpg(png_path, jpg_path):
-    img = Image.open(png_path).convert('RGB')  # Convert to RGB to avoid alpha channel issues
-    img.save(jpg_path, 'JPEG', quality=95)  # Save as JPG with quality setting
-    
